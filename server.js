06f3e06ff79fadb553084b3fde320957d9a4677f
sanseru <sanseru1@yahoo.com>
2025-01-16 08:37:38 +0700

    Add wave SVG, implement security middleware, create harga.html page, and enhance CSS styles

--- server.js
+++ server.js
@@ -2,15 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const xss = require('xss-clean');
+const helmet = require('helmet');
+
 
 // Serve static files
 app.use(express.static('public'));
+app.use(helmet());
+app.use(xss());
 
 // Serve the landing page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
+// app.get('/harga', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'harga.html'));
+// });
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
